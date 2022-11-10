b04b157b614d24e064ac8ab359fc52499e3afae2
Ahmed Bayoumy <ahmed.bayoumy@mail.mcgill.ca>
2022-11-09 21:10:20 -0500

    Prepare package

---
+++ src/SML/__init__.py
@@ -0,0 +1,3 @@
+from .SML import FileMissingError, ExceptionError, SAMPLING_METHOD, norm_t, DataSet, sampling, FullFactorial, LHS, RS, halton, surrogateModel, RBF, Kriging, bmSM
+
+__all__ = ['FileMissingError', 'ExceptionError', 'SAMPLING_METHOD', 'norm_t', 'DataSet', 'sampling', 'FullFactorial', 'LHS', 'RS', 'halton', 'surrogateModel', 'RBF', 'Kriging', 'bmSM']
