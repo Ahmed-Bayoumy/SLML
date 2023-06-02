19d62fe74043a56169d9592276947c5db48a1845
Ahmed-Bayoumy <ahmed.bayoumy@mail.mcgill.ca>
2023-06-01 21:57:01 -0400

    update-setup-file

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 if __name__ == "__main__":
-  setup()
+  setup(
+    name="SLML",
+    author="Ahmed H. Bayoumy",
+    author_email="ahmed.bayoumy@mail.mcgill.ca",
+    version='2.0.0',
+    packages=find_packages(include=['SLML', 'SLML.*']),
+    install_requires=[
+      'numpy>=1.22.4',
+      'OMADS>=1.5.0',
+      'pandas>=1.4.2',
+      'pyDOE2>=1.3.0',
+      'scikit_learn>=1.1.1',
+      'scipy>=1.8.1',
+      'setuptools>=58.1.0',
+      'requests>=2.20.0'
+    ],
+    extras_require={
+        'interactive': ['matplotlib>=3.5.2', 'plotly>=5.14.1'],
+    }
+  )
